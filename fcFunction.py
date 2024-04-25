c123b5cb1f10b94a9c28e61ae9b83fad860ef651
LuisTacla <ltacla@bairru.com.br>
2024-04-25 13:44:42 -0300

    Teste

---
+++ fcFunction.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def custom_color_map(value, p5, p90, p95):
+  if value > p95:
+    return 'black'
+  else: 
+    norm_value = (value - p5) / (p95 - p5)
+    return plt.cm.RdYlGn_r(norm_value)
+  
+  
